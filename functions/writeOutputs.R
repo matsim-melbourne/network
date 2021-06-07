1da0170670cb4debeca5ec54041a3440fdd5e223
jafshin <af.jafari.1992@gmail.com>
2021-06-08 09:48:01 +1000

    making sure we had col id in XML writing

--- functions/writeOutputs.R
+++ functions/writeOutputs.R
@@ -171,6 +171,9 @@ exportXML <- function(networkFinal, outputFileName = "outputXML"){
   
   # Links -------------------------------------------------------------------
   if(class(links)[1]=="sf") links <- st_drop_geometry(links)
+  # Adding empty id column if doesn't exist
+  if(!("id" %in% colnames(links))) links <- links %>% mutate(id=NA)
+  
   # Adding a reverse links for bi-directionals
   bi_links <- links %>% 
     filter(is_oneway==0) %>% 
